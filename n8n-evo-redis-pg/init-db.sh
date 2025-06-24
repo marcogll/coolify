1c96964a5875a0fd8fe234ad44b6f02bce34e8a0
Marco Gallegos <marco.gallegos@outlook.com>
2025-06-24 09:45:27 -0600

    Create init-db.sh

---
+++ n8n-evo-redis-pg/init-db.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE $N8N_DB_NAME;
+    CREATE DATABASE $EVOLUTION_DB_NAME;
+EOSQL
